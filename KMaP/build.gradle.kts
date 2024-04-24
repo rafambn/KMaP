1d59f4be85e5cf2cb027d13a32792ad03a751a6f
Rafael Mendonça <rafa_mbn@hotmail.com>
2024-04-24 11:06:39 -0300

    Removed unnecessary ktor libraries; Update libraries;

--- KMaP/build.gradle.kts
+++ KMaP/build.gradle.kts
@@ -50,12 +50,10 @@ kotlin {
             implementation(libs.kotlinx.coroutines.core)
 
             implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.content.negotiation)
-            implementation(libs.ktor.serialization.kotlinx.json)
         }
 
         androidMain.dependencies {
-            implementation(libs.ktor.client.android)
+            implementation(libs.ktor.client.okhttp)
         }
         jvmMain.dependencies {
             implementation(libs.ktor.client.okhttp)
--- KMaP/src/commonMain/kotlin/io/github/rafambn/kmap/KtorClient.kt
+++ KMaP/src/commonMain/kotlin/io/github/rafambn/kmap/KtorClient.kt
@@ -1,20 +1,9 @@
 package io.github.rafambn.kmap
 
 import io.ktor.client.HttpClient
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
 
 object KtorClient {
     fun provideHttpClient(): HttpClient {
-        return HttpClient {
-            install(ContentNegotiation) {
-                json(
-                    Json {
-                        prettyPrint = true
-                    }
-                )
-            }
-        }
+        return HttpClient()
     }
 }
