c70f4de072ee056a2e6fc0a4521bde5a4004c18f
Rafael Mendonça <rafa_mbn@hotmail.com>
2025-08-07 16:44:40 -0300

    Added Performance and Size tests

---
+++ Kflate/src/jvmTest/kotlin/Performance.kt
@@ -0,0 +1,206 @@
+@file:OptIn(ExperimentalUnsignedTypes::class)
+
+package com.rafambn.kflate
+
+import org.junit.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.util.zip.Deflater
+import java.util.zip.DeflaterOutputStream
+import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
+import java.util.zip.Inflater
+import java.util.zip.InflaterInputStream
+import kotlin.system.measureTimeMillis
+
+class PerformanceTest {
+
+    private val testFiles = listOf(
+        "model3D",
+        "text",
+        "Rainier.bmp",
+        "Maltese.bmp",
+        "Sunrise.bmp",
+        "simpleText",
+    )
+
+    private fun readResourceFile(fileName: String): ByteArray {
+        return javaClass.classLoader.getResourceAsStream(fileName)?.readBytes()
+            ?: throw IllegalArgumentException("Resource file not found: $fileName")
+    }
+
+    private fun ByteArray.toUByteArray(): UByteArray {
+        return UByteArray(this.size) { this[it].toUByte() }
+    }
+
+    private fun UByteArray.toByteArray(): ByteArray {
+        return ByteArray(this.size) { this[it].toByte() }
+    }
+
+    // FLATE TESTS
+    @Test
+    fun testFlateCompressPerformance() {
+        println("Flate Compress Performance")
+        println("File | KFlate Time | JVM Time")
+        println("--- | --- | ---")
+        for (fileName in testFiles) {
+            val originalData = readResourceFile(fileName)
+
+            val kflateTime = measureTimeMillis {
+                KFlate.Flate.deflate(originalData.toUByteArray())
+            }
+
+            val jvmTime = measureTimeMillis {
+                val deflater = Deflater(6, true)
+                val outputStream = ByteArrayOutputStream()
+                val deflaterStream = DeflaterOutputStream(outputStream, deflater)
+                deflaterStream.write(originalData)
+                deflaterStream.finish()
+                deflaterStream.close()
+                outputStream.toByteArray()
+            }
+            println("$fileName | ${kflateTime}ms | ${jvmTime}ms")
+        }
+    }
+
+    @Test
+    fun testFlateDecompressPerformance() {
+        println("\nFlate Decompress Performance")
+        println("File | KFlate Time | JVM Time")
+        println("--- | --- | ---")
+        for (fileName in testFiles) {
+            val originalData = readResourceFile(fileName)
+            val deflater = Deflater(6, true)
+            val outputStream = ByteArrayOutputStream()
+            val deflaterStream = DeflaterOutputStream(outputStream, deflater)
+            deflaterStream.write(originalData)
+            deflaterStream.finish()
+            deflaterStream.close()
+            val compressedData = outputStream.toByteArray()
+
+            val kflateTime = measureTimeMillis {
+                KFlate.Flate.inflate(compressedData.toUByteArray())
+            }
+
+            val jvmTime = measureTimeMillis {
+                val inflater = Inflater(true)
+                val inputStream = ByteArrayInputStream(compressedData)
+                val inflaterStream = InflaterInputStream(inputStream, inflater)
+                val decompressedStream = ByteArrayOutputStream()
+                inflaterStream.copyTo(decompressedStream)
+                inflater.end()
+            }
+            println("$fileName | ${kflateTime}ms | ${jvmTime}ms")
+        }
+    }
+
+    // GZIP TESTS
+    @Test
+    fun testGzipCompressPerformance() {
+        println("\nGzip Compress Performance")
+        println("File | KFlate Time | JVM Time")
+        println("--- | --- | ---")
+        for (fileName in testFiles) {
+            val originalData = readResourceFile(fileName)
+
+            val kflateTime = measureTimeMillis {
+                KFlate.Gzip.compress(originalData.toUByteArray())
+            }
+
+            val jvmTime = measureTimeMillis {
+                val outputStream = ByteArrayOutputStream()
+                val gzipOutputStream = GZIPOutputStream(outputStream)
+                gzipOutputStream.write(originalData)
+                gzipOutputStream.finish()
+                gzipOutputStream.close()
+                outputStream.toByteArray()
+            }
+            println("$fileName | ${kflateTime}ms | ${jvmTime}ms")
+        }
+    }
+
+    @Test
+    fun testGzipDecompressPerformance() {
+        println("\nGzip Decompress Performance")
+        println("File | KFlate Time | JVM Time")
+        println("--- | --- | ---")
+        for (fileName in testFiles) {
+            val originalData = readResourceFile(fileName)
+            val outputStream = ByteArrayOutputStream()
+            val gzipOutputStream = GZIPOutputStream(outputStream)
+            gzipOutputStream.write(originalData)
+            gzipOutputStream.finish()
+            gzipOutputStream.close()
+            val compressedData = outputStream.toByteArray()
+
+            val kflateTime = measureTimeMillis {
+                KFlate.Gzip.decompress(compressedData.toUByteArray())
+            }
+
+            val jvmTime = measureTimeMillis {
+                val inputStream = ByteArrayInputStream(compressedData)
+                val gzipInputStream = GZIPInputStream(inputStream)
+                val decompressedStream = ByteArrayOutputStream()
+                gzipInputStream.copyTo(decompressedStream)
+            }
+            println("$fileName | ${kflateTime}ms | ${jvmTime}ms")
+        }
+    }
+
+    // ZLIBTESTS
+    @Test
+    fun testZlibCompressPerformance() {
+        println("\nZlib Compress Performance")
+        println("File | KFlate Time | JVM Time")
+        println("--- | --- | ---")
+        for (fileName in testFiles) {
+            val originalData = readResourceFile(fileName)
+
+            val kflateTime = measureTimeMillis {
+                KFlate.Zlib.compress(originalData.toUByteArray())
+            }
+
+            val jvmTime = measureTimeMillis {
+                val deflater = Deflater(Deflater.DEFAULT_COMPRESSION)
+                val outputStream = ByteArrayOutputStream()
+                val deflaterStream = DeflaterOutputStream(outputStream, deflater)
+                deflaterStream.write(originalData)
+                deflaterStream.finish()
+                deflaterStream.close()
+                outputStream.toByteArray()
+            }
+            println("$fileName | ${kflateTime}ms | ${jvmTime}ms")
+        }
+    }
+
+    @Test
+    fun testZlibDecompressPerformance() {
+        println("\nZlib Decompress Performance")
+        println("File | KFlate Time | JVM Time")
+        println("--- | --- | ---")
+        for (fileName in testFiles) {
+            val originalData = readResourceFile(fileName)
+            val deflater = Deflater(Deflater.DEFAULT_COMPRESSION)
+            val outputStream = ByteArrayOutputStream()
+            val deflaterStream = DeflaterOutputStream(outputStream, deflater)
+            deflaterStream.write(originalData)
+            deflaterStream.finish()
+            deflaterStream.close()
+            val compressedData = outputStream.toByteArray()
+
+            val kflateTime = measureTimeMillis {
+                KFlate.Zlib.decompress(compressedData.toUByteArray())
+            }
+
+            val jvmTime = measureTimeMillis {
+                val inflater = Inflater()
+                val inputStream = ByteArrayInputStream(compressedData)
+                val inflaterStream = InflaterInputStream(inputStream, inflater)
+                val decompressedStream = ByteArrayOutputStream()
+                inflaterStream.copyTo(decompressedStream)
+                inflater.end()
+            }
+            println("$fileName | ${kflateTime}ms | ${jvmTime}ms")
+        }
+    }
+}
---
+++ Kflate/src/jvmTest/kotlin/Size.kt
@@ -0,0 +1,92 @@
+@file:OptIn(ExperimentalUnsignedTypes::class)
+
+package com.rafambn.kflate
+
+import org.junit.Test
+import java.io.ByteArrayOutputStream
+import java.util.zip.Deflater
+import java.util.zip.DeflaterOutputStream
+import java.util.zip.GZIPOutputStream
+
+class SizeTest {
+
+    private val testFiles = listOf(
+        "model3D",
+        "text",
+        "Rainier.bmp",
+        "Maltese.bmp",
+        "Sunrise.bmp",
+        "simpleText",
+    )
+
+    private fun readResourceFile(fileName: String): ByteArray {
+        return javaClass.classLoader.getResourceAsStream(fileName)?.readBytes()
+            ?: throw IllegalArgumentException("Resource file not found: $fileName")
+    }
+
+    private fun ByteArray.toUByteArray(): UByteArray {
+        return UByteArray(this.size) { this[it].toUByte() }
+    }
+
+    @Test
+    fun testFlateSize() {
+        println("Flate Size Test")
+        for (fileName in testFiles) {
+            val originalData = readResourceFile(fileName)
+            val compressedData = KFlate.Flate.deflate(originalData.toUByteArray(), DeflateOptions())
+
+            val deflater = Deflater(6, true)
+            val outputStream = ByteArrayOutputStream()
+            val deflaterStream = DeflaterOutputStream(outputStream, deflater)
+
+            deflaterStream.write(originalData)
+            deflaterStream.finish()
+            deflaterStream.close()
+
+            val expectedCompressedSize = outputStream.toByteArray().size
+
+            println("File: $fileName, KFlate: ${compressedData.size}, JVM: $expectedCompressedSize")
+        }
+    }
+
+    @Test
+    fun testGzipSize() {
+        println("Gzip Size Test")
+        for (fileName in testFiles) {
+            val originalData = readResourceFile(fileName)
+            val compressedData = KFlate.Gzip.compress(originalData.toUByteArray(), GzipOptions())
+
+            val outputStream = ByteArrayOutputStream()
+            val gzipOutputStream = GZIPOutputStream(outputStream)
+
+            gzipOutputStream.write(originalData)
+            gzipOutputStream.finish()
+            gzipOutputStream.close()
+
+            val expectedCompressedSize = outputStream.toByteArray().size
+
+            println("File: $fileName, KFlate: ${compressedData.size}, JVM: $expectedCompressedSize")
+        }
+    }
+
+    @Test
+    fun testZlibSize() {
+        println("Zlib Size Test")
+        for (fileName in testFiles) {
+            val originalData = readResourceFile(fileName)
+            val compressedData = KFlate.Zlib.compress(originalData.toUByteArray(), DeflateOptions())
+
+            val deflater = Deflater(Deflater.DEFAULT_COMPRESSION)
+            val outputStream = ByteArrayOutputStream()
+            val deflaterStream = DeflaterOutputStream(outputStream, deflater)
+
+            deflaterStream.write(originalData)
+            deflaterStream.finish()
+            deflaterStream.close()
+
+            val expectedCompressedSize = outputStream.toByteArray().size
+
+            println("File: $fileName, KFlate: ${compressedData.size}, JVM: $expectedCompressedSize")
+        }
+    }
+}
