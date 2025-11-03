b375cc33292776e1f272291be5ba1c961f581780
Rafael Mendonça <rafa_mbn@hotmail.com>
2025-11-03 19:07:46 -0300

    Added some code for debugging

--- KMaPDemo/src/commonMain/kotlin/com/rafambn/kmap/customSources/VectorTileSource.kt
+++ KMaPDemo/src/commonMain/kotlin/com/rafambn/kmap/customSources/VectorTileSource.kt
@@ -15,6 +15,7 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.protobuf.ProtoBuf
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.InternalResourceApi
+import org.jetbrains.compose.resources.readResourceBytes
 
 class VectorTileSource : VectorTileSource {
     private val client = HttpClient()
@@ -26,7 +27,7 @@ class VectorTileSource : VectorTileSource {
     }
 
     @OptIn(ExperimentalResourceApi::class, InternalResourceApi::class, ExperimentalUnsignedTypes::class, ExperimentalSerializationApi::class)
-    override suspend fun getTile(zoom: Int, row: Int, column: Int): VectorTileResult {
+    override suspend fun getTile(zoom: Int, row: Int, column: Int): VectorTileResult { //TODO maybe row and column are inverted
         try {
 //            val compressedBytes = client.get("https://vtiles.openhistoricalmap.org/maps/osm/$zoom/$row/$column") {
 //                contentType(ContentType.Application.ProtoBuf)
@@ -34,11 +35,17 @@ class VectorTileSource : VectorTileSource {
 //            val rawMVTile = ProtoBuf.decodeFromByteArray(RawMVTile.serializer(), compressedBytes)
 //            val mvTile = rawMVTile.parse()
 
-            val compressedBytes = client.get("https://api.maptiler.com/tiles/v3-openmaptiles/$zoom/$row/$column.pbf?key=GCqxEKWuBP1S6iQ1aSBG") {
+            val compressedBytes = client.get("https://api.maptiler.com/tiles/v4/$zoom/$row/$column.pbf?key=GCqxEKWuBP1S6iQ1aSBG") {
                 contentType(ContentType.Application.ProtoBuf)
             }.readRawBytes()
-            val rawMVTile = ProtoBuf.decodeFromByteArray(RawMVTile.serializer(), compressedBytes)
+//            val rawMVTile = ProtoBuf.decodeFromByteArray(RawMVTile.serializer(), compressedBytes)
+            val rawMVTile = json.decodeFromString(RawMVTile.serializer(), readResourceBytes("tile.json").decodeToString())
             val mvTile = rawMVTile.parse()
+            println("$zoom/$row/$column")
+//            if (zoom == 2 && row == 0 && column == 3){
+//                val jsonTile = Json.encodeToString(RawMVTile.serializer(),ProtoBuf.decodeFromByteArray(RawMVTile.serializer(), compressedBytes))
+//                println(jsonTile)
+//            }
 
 //            val compressedBytes = client.get("https://tiles.versatiles.org/tiles/osm/$zoom/$row/$column") {
 //                contentType(ContentType.Application.ProtoBuf)
@@ -51,6 +58,8 @@ class VectorTileSource : VectorTileSource {
             return VectorTileResult.Success(VectorTile(zoom, row, column, mvTile))
         } catch (ex: Exception) {
             println(ex)
+            println(ex.cause)
+            println("Tile $zoom/$row/$column failed")
         }
         return VectorTileResult.Failure(TileSpecs(zoom, row, column))
     }
--- KMaPDemo/src/commonMain/kotlin/com/rafambn/kmap/screens/VectorTileScreen.kt
+++ KMaPDemo/src/commonMain/kotlin/com/rafambn/kmap/screens/VectorTileScreen.kt
@@ -6,23 +6,27 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.rafambn.kmap.components.VectorCanvasParameters
 import com.rafambn.kmap.core.KMaP
 import com.rafambn.kmap.core.rememberMapState
 import com.rafambn.kmap.customSources.OSMMapProperties
+import com.rafambn.kmap.customSources.SimpleZoomLevelRange
 import com.rafambn.kmap.customSources.VectorTileSource
 import com.rafambn.kmap.getGestureDetector
 import com.rafambn.kmap.mapProperties.border.BoundMapBorder
 import com.rafambn.kmap.mapProperties.border.MapBorderType
 import com.rafambn.kmap.mapProperties.border.OutsideTilesType
-import com.rafambn.kmap.styleBasic
 import com.rafambn.kmap.utils.style.Style
 import kmap.kmapdemo.generated.resources.Res
 import kmap.kmapdemo.generated.resources.back_arrow
 import kotlinx.serialization.json.Json
 import org.jetbrains.compose.resources.InternalResourceApi
+import org.jetbrains.compose.resources.readResourceBytes
 import org.jetbrains.compose.resources.vectorResource
 
 @OptIn(InternalResourceApi::class)
@@ -33,7 +37,8 @@ fun VectorTileScreen(
     val mapState = rememberMapState(
         mapProperties = OSMMapProperties(
             boundMap = BoundMapBorder(horizontal = MapBorderType.BOUND, vertical = MapBorderType.BOUND),
-            outsideTiles = OutsideTilesType.NONE
+            outsideTiles = OutsideTilesType.NONE,
+            zoomLevels = SimpleZoomLevelRange(max = 14)
         )
     )
     val json = Json {
@@ -41,22 +46,30 @@ fun VectorTileScreen(
         isLenient = true
         useArrayPolymorphism = false
     }
-    val style = json.decodeFromString<Style>(styleBasic)
-    Box {
-        KMaP(
-            modifier = Modifier.fillMaxSize(),
-            mapState = mapState,
-        ) {
-            vectorCanvas(
-                parameters = VectorCanvasParameters(id = 1, tileSource = VectorTileSource()::getTile, style = style),
-                gestureWrapper = getGestureDetector(mapState.motionController)
+    val styleState = remember { mutableStateOf<Style?>(null) }
+
+    LaunchedEffect(Unit) {
+        val styleJson = readResourceBytes("style.json").decodeToString()
+        styleState.value = json.decodeFromString<Style>(styleJson)
+    }
+
+    styleState.value?.let { style ->
+        Box {
+            KMaP(
+                modifier = Modifier.fillMaxSize(),
+                mapState = mapState,
+            ) {
+                vectorCanvas(
+                    parameters = VectorCanvasParameters(id = 1, tileSource = VectorTileSource()::getTile, style = style),
+                    gestureWrapper = getGestureDetector(mapState.motionController)
+                )
+            }
+            Image(
+                imageVector = vectorResource(Res.drawable.back_arrow),
+                contentDescription = "",
+                modifier = Modifier.clickable { navigateBack() }
+                    .size(70.dp)
             )
         }
-        Image(
-            imageVector = vectorResource(Res.drawable.back_arrow),
-            contentDescription = "",
-            modifier = Modifier.clickable { navigateBack() }
-                .size(70.dp)
-        )
     }
 }
